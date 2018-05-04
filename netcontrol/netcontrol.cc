d25b26422b0235c625d9943809e8fc060e5f4d3b
Tatiana Ermakova <t.ermakova@securitycode.ru>
2018-05-04 13:38:18 +0300

    Добавила hh для основного класса
    Могут возникнуть проблемы с реджистром

--- netcontrol/netcontrol.cc
+++ netcontrol/netcontrol.cc
@@ -1,4 +1,4 @@
-#include <network.hh>
+#include <netcontrol.hh>
 
 
 #include <boost/bind.hpp>
@@ -8,75 +8,75 @@
 using namespace vigil;
 using namespace vigil::container;
 
-class netcontrol : public Component {
-        Network network;
-        uint32_t switches_num;
+Netcontrol::Netcontrol(const Context* c, const json_object*)
+        : Component(c), network(init()), switches_num(0)
+{}
+
+Network Netcontrol::init()
+{
+        std::vector<Host> hosts;
+        hosts.push_back(Host());
+        hosts.push_back(Host());
+        hosts.push_back(Host());
+        return Network(Algorithm().initial(), hosts, this);
+}
 
 
-        Network init() {
-                std::vector<Host> hosts;
-                hosts.push_back(Host());
-                hosts.push_back(Host());
-                hosts.push_back(Host());
-                return Network(Algorithm().initial(), hosts, this);
-        }
+void Netcontrol::scenario()
+{
+        std::cout << "LOG: Starting scenario" << std::endl;
+}
 
+void Netcontrol::apply() {
+        std::cout << "LOG: New configuration applied" << std::endl;
+}
 
-        void scenario() {
-                std::cout << "LOG: Starting scenario" << std::endl;
-        }
+void Netcontrol::breakComm()
+{
+	std::cout << "LOG: Commutator broken" << std::endl;
+	map.breakComm();
+	reload();
+}
 
-	void apply() {
-                std::cout << "LOG: New configuration applied" << std::endl;
-        }
+void Netcontrol::breakLink()
+{
+	std::cout << "LOG: Link broken" << std::endl;
+	map.breakLink();
+	reload();
+}
 
-	void reload()
-	{
-		VlSet vls;
-		try {
-			vls = Algotithm.run();
-		catch (const exception &e) {
-			std::cout << "LOG: New VL's configuration can't be created" << std::endl;
-			return;
-		}
-	        std::cout << "LOG: New VL's configuration created" << std::endl;
-		apply(vls);
+void Netcontrol::reload()
+{
+	VlSet vls;
+	try {
+		vls = Algotithm.run();
+	} catch (const exception &e) {
+		std::cout << "LOG: New VL's configuration can't be created" << std::endl;
+		return;
 	}
-public:
-        netcontrol(const Context* c, const json_object*)
-                : Component(c), network(init()), switches_num(0)
-        {}
+	std::cout << "LOG: New VL's configuration created" << std::endl;
+	apply(vls);
+}
 
-        void configure(const Configuration*) {
+void Netcontrol::configure(const Configuration*)
+{
+}
+
+void Netcontrol::install()
+{
+        register_handler(Datapath_join_event::static_get_name(),
+		boost::bind(&netcontrol::handler, this, _1));
+}
+
+Disposition Netcontrol::handler(const Event& e)
+{
+        switches_num++;
+        if (switches_num != network.switchesAmount()) {
+                return CONTINUE;
         }
+        scenario();
+        return STOP;
+}
 
-        void install() {
-                register_handler(Datapath_join_event::static_get_name(), boost::bind(&netcontrol::handler, this, _1));
-        }
 
-        Disposition handler (const Event& e) {
-                switches_num++;
-                if (switches_num != network.switchesAmount()) {
-                        return CONTINUE;
-                }
-                scenario();
-                return STOP;
-        }
-
-	void breakComm()
-	{
-		std::cout << "LOG: Commutator broken" << std::endl;
-		map.breakComm();
-		reload();
-	}
-
-	void breakLink()
-	{
-		std::cout << "LOG: Link broken" << std::endl;
-		map.breakLink();
-		reload();
-	}
-
-};
-
-REGISTER_COMPONENT(container::Simple_component_factory<netcontrol>, netcontrol);
+REGISTER_COMPONENT(container::Simple_component_factory<Netcontrol>, Netcontrol);
---
+++ netcontrol/netcontrol.hh
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <network.hh>
+
+
+#include <boost/bind.hpp>
+#include <unistd.h>
+#include "datapath-join.hh"
+
+using namespace vigil;
+using namespace vigil::container;
+
+class Netcontrol : public Component {
+public:
+        Netcontrol(const Context* c, const json_object*);
+
+        void configure(const Configuration*);
+        void install();
+        Disposition handler (const Event& e);
+	void breakComm();
+	void breakLink();
+private:
+	Network network;
+	uint32_t switches_num;
+
+
+	Network init();
+	void scenario();
+	void apply();
+	void reload();
+
+};
