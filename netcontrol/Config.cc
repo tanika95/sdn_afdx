c41c9eb9ebd732b2ddf3d54d1cfec8bd48853beb
Tatiana <tanyaerm@mail.ru>
2018-05-05 17:19:01 +0300

    Добавлен разбор конфига

---
+++ netcontrol/Config.cc
@@ -0,0 +1,35 @@
+#include "Config.hh"
+
+#include <vector>
+#include <iostream>
+#include <boost/foreach.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include "Sla.hh"
+#include "Vl.hh"
+
+using namespace std;
+using namespace boost::property_tree;
+
+Config::Config(const string &file)
+{
+	read_xml(file, tree);
+}
+
+Topology Config::topology()
+{
+}
+
+VLSet Config::dataflows()
+{
+	VLSet vls;
+	BOOST_FOREACH(ptree::value_type &vl, tree.get_child("links")) {
+        	uint32_t id = vl.second.get<uint32_t>("vl.id");
+		uint32_t sender = vl.second.get<uint32_t>("vl.sender");
+		uint32_t reciever = vl.second.get<uint32_t>("vl.reciever");
+		double bag = vl.second.get<uint32_t>("vl.bag");
+		double lmax = vl.second.get<uint32_t>("vl.lmax");
+		double jitt = vl.second.get<uint32_t>("vl.jitter");
+		vls.add(VL(id, sender, reciever, SLA(bag, lmax, jitt)));
+	}
+	return vls;
+}
---
+++ netcontrol/Config.hh
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include <boost/property_tree/ptree.hpp>
+
+#include "Topology.hh"
+#include "Vlset.hh"
+
+class Config
+{
+public:
+	Config(const std::string &file);
+	Topology topology();
+	VLSet dataflows();
+private:
+	boost::property_tree::ptree tree;
+};
